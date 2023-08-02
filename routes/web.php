d9e2994765cdb1db34d77916ad1da06af369df72
carloeusebi <carloeusebi@gmail.com>
2023-08-02 17:12:16 +0200

    Added more routes

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/contacts', function () {
+    return view('contacts');
+})->name('contacts');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
